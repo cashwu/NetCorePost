c7ca4fc0ff5dccc58b3964d379a2010771f82ae0
cash <cash@cashwu.com>
2020-03-27 15:31:43 +0800

    init

---
+++ testNetCore3/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace testNetCore3.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+        
+        [Route("/Test")]
+        [HttpPost]
+        public IActionResult Index([FromBody]UserModel userModel)
+        {
+            _logger.LogInformation($" net core 3 -- {userModel}"); 
+            
+            return Ok();
+        }
+    }
+
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Name)}: {Name}";
+        }
+    }
+}
